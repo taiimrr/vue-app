94e46ccfcf661369a9b788af82b3edb98cce179e
Taimoor <taimoor.nasir@mailbox.tu-dresden.de>
2024-01-21 00:10:04 +0100

    basic structure

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2>Expense Tracker</h2>
+</template>
+
+<!-- For JS integration into the file 
+
+<script></script>
+
+For styling (scoped means it will only affect the current file) 
+
+<style scoped></style>
+
+
+-->
